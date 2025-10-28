f1f451e8f895f62772961eeb7e513df082dd50b1
Mateus Aparecido Gianelli Wada <44754894820@>
2025-10-28 11:36:11 -0300

    teste

--- src/Paginas/Autor.jsx
+++ src/Paginas/Autor.jsx
@@ -1,6 +1,8 @@
+import estilo from './Autor.module.css';
+
 export function Autor() {
     return (
-        <main>
+        <main className={estilo.fundo}>
             <h1>Sherlock</h1>
         </main>
     )
